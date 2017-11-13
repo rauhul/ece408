a62b7408506cf6877605f962fcb57d1151768aa2
rauhul <rauhul.varma@gmail.com>
2017-11-13 16:28:50 -0600

    milestone2 code

--- project/ece408_src/new-forward.h
+++ project/ece408_src/new-forward.h
@@ -4,42 +4,42 @@
 
 #include <mxnet/base.h>
 
-namespace mxnet
-{
-namespace op
-{
+namespace mxnet {
+    namespace op {
 
-// This function is called by new-inl.h
-// Any code you write should be executed by this function
-template <typename cpu, typename DType>
-void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DType> &x, const mshadow::Tensor<cpu, 4, DType> &k)
-{
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    The code in 16 is for a single image.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct, not fast (this is the CPU implementation.)
-    */
+        // This function is called by new-inl.h
+        // Any code you write should be executed by this function
+        template <typename cpu, typename DType>
+        void forward(mshadow::Tensor<cpu, 4, DType> &y,
+               const mshadow::Tensor<cpu, 4, DType> &x,
+               const mshadow::Tensor<cpu, 4, DType> &k) {
 
-    
-    const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = w.shape_[3];
+            /**
+             *  Modify this function to implement the forward pass described in Chapter 16.
+             *  The code in 16 is for a single image.
+             *  We have added an additional dimension to the tensors to support an entire mini-batch
+             *  The goal here is to be correct, not fast (this is the CPU implementation.)
+             */
 
-    for (int b = 0; b < B; ++b) {
-        CHECK_EQ(0, 1) << "Missing an ECE408 CPU implementation!";
+            const int B = x.shape_[0];
+            const int M = y.shape_[1];
+            const int C = x.shape_[1];
+            const int H = x.shape_[2];
+            const int W = x.shape_[3];
+            const int K = k.shape_[3];
 
-        /* ... a bunch of nested loops later...
-            y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
-        */
+            for (int b = 0; b < B; ++b)                     // for each image in the batch
+                for (int m = 0; m < M; ++m)                 // for each output feature maps
+                    for (int h = 0; h < H; ++h)             // for each output element
+                        for (int w = 0; w < W; ++w) {
+                            y[b][m][h][w] = 0;
+                            for (int c = 0; c < C; ++c)     // sum over all input feature maps
+                                for (int p = 0; p < K; ++p) // KxK filter
+                                    for (int q = 0; q < K; ++q)
+                                        y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+                        }
+        }
     }
-
-
-}
-}
 }
 
 #endif
