6584b6a32ed50b97cc503cbfb825c20b88ef35f2
rauhul <rauhul.varma@gmail.com>
2017-12-06 03:03:33 -0600

    simple shared memory

--- project/ece408_src/new-forward.cuh
+++ project/ece408_src/new-forward.cuh
@@ -3,47 +3,41 @@
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 
 #include <mxnet/base.h>
-#define TILE_WIDTH 16
+#define TILE_WIDTH 28
 
 namespace mxnet
 {
 namespace op
 {
 
+// __constant__ float *kernels;
 
 template<typename gpu, typename DType>
-__global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int B, const int M, const int C, const int H, const int W, const int K) {
+__global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int B, const int M, const int H, const int W, const int K) {
 
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
+    const int H_out = 24;
+    const int W_out = 24;
 
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
     #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
-    #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
-    #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
+    #define x4d(i3,i1,i0)    x[(i3) * (H * W) + (i1)*(W) + i0]
+    #define k4d(i3,i1,i0)    k[(i3) * (K * K) + (i1)*(K) + i0]
 
-    const int W_grid = ceil(W_out / (double) TILE_WIDTH);
+    int n = blockIdx.x;
+    int m = blockIdx.y;
+    int h = threadIdx.y;
+    int w = threadIdx.x;
 
-    int n, m, h, w, c, p, q;
-    n = blockIdx.x;
-    m = blockIdx.y;
-    h = (blockIdx.z / W_grid) * blockDim.y + threadIdx.y;
-    w = (blockIdx.z % W_grid) * blockDim.x + threadIdx.x;
+
+    __shared__ float tileCache[TILE_WIDTH][TILE_WIDTH];
+    tileCache[h][w] = x4d(n, h, w);
+    __syncthreads();
 
     if(h < H_out && w < W_out) {
         float acc = 0;
 
-        for(c = 0; c < C; c++)
-            for(p = 0; p < K; p++)
-                for(q = 0; q < K; q++)
-                    acc += x4d(n, c, h+p, w+q) * k4d(m, c, p, q);
+        for(int p = 0; p < K; ++p)
+            for(int q = 0; q < K; ++q)
+                acc += tileCache[h+p][w+q] * k4d(m, p, q);
 
         y4d(n,m,h,w) = acc;
     }
@@ -61,6 +55,39 @@ __global__ void forward_kernel(DType *y, const DType *x, const DType *k, const i
 template<typename gpu, typename DType>
 void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w) {
 
+//     std::cout << "x" << std::endl;
+//     std::cout << "x.shape_[0]" << x.shape_[0] << std::endl;
+//     std::cout << "x.shape_[1]" << x.shape_[1] << std::endl;
+//     std::cout << "x.shape_[2]" << x.shape_[2] << std::endl;
+//     std::cout << "x.shape_[3]" << x.shape_[3] << std::endl;
+
+//     std::cout << "w" << std::endl;
+//     std::cout << "w.shape_[0]" << w.shape_[0] << std::endl;
+//     std::cout << "w.shape_[1]" << w.shape_[1] << std::endl;
+//     std::cout << "w.shape_[2]" << w.shape_[2] << std::endl;
+//     std::cout << "w.shape_[3]" << w.shape_[3] << std::endl;
+
+//     std::cout << "y" << std::endl;
+//     std::cout << "y.shape_[0]" << y.shape_[0] << std::endl;
+//     std::cout << "y.shape_[1]" << y.shape_[1] << std::endl;
+//     std::cout << "y.shape_[2]" << y.shape_[2] << std::endl;
+//     std::cout << "y.shape_[3]" << y.shape_[3] << std::endl;
+
+// x
+// x.shape_[0]10000
+// x.shape_[1]1
+// x.shape_[2]28
+// x.shape_[3]28
+// w
+// w.shape_[0]50
+// w.shape_[1]1
+// w.shape_[2]5
+// w.shape_[3]5
+// y
+// y.shape_[0]10000
+// y.shape_[1]50
+// y.shape_[2]24
+// y.shape_[3]24
 
     // Use mxnet's CHECK_EQ to do assertions.
     //CHECK_EQ(0, 1) << "Missing an ECE408 GPU implementation!";
@@ -83,17 +110,12 @@ void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DT
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    const int W_grid = ceil(W_out / (double) TILE_WIDTH);
-    const int H_grid = ceil(H_out / (double) TILE_WIDTH);
-    const int Z = H_grid * W_grid;
-
-    // printf("Values: %i,%i,%i,%i,%i\n", H_out, W_out, W_grid, H_grid, Z);
 
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 gridDim(B, M, Z);
+    dim3 gridDim(B, M, 1);
 
     // Call the kernel
-    forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
